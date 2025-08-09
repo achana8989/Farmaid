124c0a9962a87468370681c5a468ee8e8cf2bfda
achana8989 <73003826+achana8989@users.noreply.github.com>
2025-08-09 00:33:52 +0000

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,7 +1,16 @@
-{
-  "expo": {
-    "name": "Farmaid",
-    "slug": "farmaid",
-    "version": "1.0.0"
-  }
-}
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CaptureScreen from './components/CaptureScreen';
+import ResultScreen from './components/ResultScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Farmaid - Capture" component={CaptureScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer
